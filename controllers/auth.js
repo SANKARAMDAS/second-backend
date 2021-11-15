8bd0c02bcaae0df0d9fce3cfc877f05be9519304
sanchi-shirur4 <sanchi.shirur4@gmail.com>
2021-11-15 11:49:27 +0530

    Google Auth Integration

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,26 +1,41 @@
 const { OAuth2Client } = require("google-auth-library");
-
 const User = require("../models/user");
 
 const client = new OAuth2Client(process.env.CLIENT_ID);
 
 const googleLogin = async (req, res) => {
-	const { tokenId } = req.body;
-	client
-		.verifyIdToken({
-			idToken: tokenId,
-			audience: process.env.CLIENT_ID,
-		})
-		.then((response) => {
-			const { email_verified, email, name } = response.payload;
-			res.send(email_verified, email, name);
-			if (email_verified) {
-				// if verified?
-				// check if email exists in db
-				// of yes? only login
-				// if no? add to db
-			}
-		});
+	const { tokenId, role } = req.body;
+
+	const ticket = await client.verifyIdToken({
+		idToken: tokenId,
+		audience: process.env.CLIENT_ID,
+	});
+
+	const payload = ticket.getPayload();
+
+	console.log("PAYLOAD", payload["email"]);
+
+	if (payload["email_verified"]) {
+		const userExists = await User.findOne({ email: payload["email"] });
+		console.log("user ex", userExists);
+		if (userExists) {
+			err = "Email already exists";
+			res.send(err);
+		} else {
+			const user = await new User({
+				name: payload["name"],
+				email: payload["email"],
+				role,
+			});
+
+			const savedUser = await user.save();
+			const token = await user.generateAuthToken();
+			req.session.token = token;
+			res.status(200).send(savedUser);
+		}
+	} else {
+		res.send("There was an error accessing your google account.");
+	}
 };
 
 module.exports = {
--- models/user.js
+++ models/user.js
@@ -1,83 +1,87 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 const express = require("express");
 var router = express.Router();
-const bcrypt = require('bcrypt')
-const jwt = require('jsonwebtoken')
-const validator = require('validator')
+const bcrypt = require("bcrypt");
+const jwt = require("jsonwebtoken");
+const validator = require("validator");
 
 // User Schema
 const UserSchema = new mongoose.Schema({
-    name: {
-        type: String,
-        trim: true,
-        required: true
-    },
-    email: {
-        type: String,
-        trim: true,
-        required: true,
-        validate(value) {
-            if (!validator.isEmail(value)) {
-                throw new Error('not an email')
-            }
-        }
-    },
-    password: {
-        type: String,
-        trim: true,
-        required: true
-    },
-    company: {
-        type: String,
-        trim: true,
-        required: false
-    },
-    ethereumWallet: {
-        type: String,
-        trim: true,
-        required: false
-    },
-    role: {
-        type: String,
-        required: true
-    },
-    isProfileComplete: {
-        type: Boolean,
-        default: false
-    },
-    tokens: [{
-        token: {
-            type: String,
-            required: true
-        }
-    }]
+	name: {
+		type: String,
+		trim: true,
+		required: true,
+	},
+	email: {
+		type: String,
+		trim: true,
+		required: true,
+		validate(value) {
+			if (!validator.isEmail(value)) {
+				throw new Error("not an email");
+			}
+		},
+	},
+	password: {
+		type: String,
+		trim: true,
+	},
+	company: {
+		type: String,
+		trim: true,
+		required: false,
+	},
+	ethereumWallet: {
+		type: String,
+		trim: true,
+		required: false,
+	},
+	role: {
+		type: String,
+		required: true,
+	},
+	isProfileComplete: {
+		type: Boolean,
+		default: false,
+	},
+	tokens: [
+		{
+			token: {
+				type: String,
+				required: true,
+			},
+		},
+	],
 });
 
 UserSchema.methods.generateAuthToken = async function () {
-    const user = this
-    const token = jwt.sign({ _id: user._id.toString() }, process.env.VERIFY_TOKEN)
+	const user = this;
+	const token = jwt.sign(
+		{ _id: user._id.toString() },
+		process.env.VERIFY_TOKEN
+	);
 
-    user.tokens = user.tokens.concat({ token })
-    user.save()
-    return token
-}
+	user.tokens = user.tokens.concat({ token });
+	user.save();
+	return token;
+};
 
 UserSchema.statics.findByCredentials = async (email, password) => {
-    const user = await User.findOne({ email })
+	const user = await User.findOne({ email });
 
-    if (!user) {
-        throw new Error('no user exists with this email')
-    }
+	if (!user) {
+		throw new Error("no user exists with this email");
+	}
 
-    const isMatch = await bcrypt.compare(password, user.password)
+	const isMatch = await bcrypt.compare(password, user.password);
 
-    if (!isMatch) {
-        throw new Error('unable to login!')
-    }
-    return user
-}
+	if (!isMatch) {
+		throw new Error("unable to login!");
+	}
+	return user;
+};
 
 mongoose.pluralize(null);
-const User = mongoose.model('User', UserSchema);
+const User = mongoose.model("User", UserSchema);
 
 module.exports = User;
