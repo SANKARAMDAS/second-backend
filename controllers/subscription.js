64723ae5e11ea1b510511cc869f756c7c7025259
unknown <tarang.padia2@gmail.com>
2021-11-16 08:08:20 +0530

    MailChimp Changes

--- controllers/subscription.js
+++ controllers/subscription.js
@@ -8,33 +8,9 @@ const mailchimp = new Mailchimp(mc_api_key)
 const addMember = async (req, res) => {
 
     const email = req.body.email
-    const FNAME = req.body.FNAME
-    const LNAME = req.body.LNAME
-    const BIRTHDAY = req.body.BIRTHDAY
-    const addr1 = req.body.addr1
-    const city = req.body.city
-    const state = req.body.state
-    const zip = req.body.zip
-    const PHONE = req.body.PHONE
-    const country = req.body.country
-
-
     mailchimp.post(`/lists/${list_id}/members/`, {
         email_address: email,
         status: "subscribed",
-        merge_fields: {
-            FNAME,
-            LNAME,
-            BIRTHDAY,
-            PHONE,
-            ADDRESS: {
-                addr1,
-                city,
-                country,
-                state,
-                zip,
-            },
-        },
     }).then((result) => {
         res.send({
             result,
