3a20923585d77f81aae056912a354ccd2416f9b5
sanchi-shirur4 <sanchi.shirur4@gmail.com>
2021-12-21 23:10:58 +0530

    Dual Login Google auth

--- routes/auth/googleLogin.js
+++ routes/auth/googleLogin.js
@@ -2,9 +2,12 @@ const express = require("express");
 const {
 	googleLogin,
 	googleSignup,
+	verifyEmailGoogleAuth,
 } = require("../../controllers/auth/googleAuth");
 
 const router = express.Router();
+
+router.post("/verifyEmailGoogleAuth", verifyEmailGoogleAuth);
 
 router.post("/googleLogin", googleLogin);
 
