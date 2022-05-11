41519a56c3d955af8fa22c19b6a665186f9a177f
DSankaram <sankaram@octaloop.com>
2022-05-11 15:18:10 +0530

    first

--- server.js
+++ server.js
@@ -51,7 +51,7 @@ db.on("error", console.error.bind(console, "connection error"));
 db.once("open", function () { });
 
 // Middlewares
-var allowedDomains = ['https://binamite.com', 'https://rdx.binamite.com', 'http://localhost:3000', 'https://beta.binamite.com'];
+var allowedDomains = ['https://binamite.com', 'https://rdx.binamite.com', 'http://localhost:3003', 'https://beta.binamite.com'];
 app.use(cors({
 	origin: function (origin, callback) {
 		if (!origin) return callback(null, true);
